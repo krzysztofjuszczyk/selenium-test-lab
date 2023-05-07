224590a538f0fea0d2250fc7c10d0f2111028516
Krzysztof Juszczyk <kjuszczyk@gazeta.pl>
2023-05-07 14:02:17 +0200

    add log

--- src/test/java/Lab_14_Menu_Test.java
+++ src/test/java/Lab_14_Menu_Test.java
@@ -1,9 +1,16 @@
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 
+import java.util.logging.Logger;
+
 public class Lab_14_Menu_Test extends SeleniumBaseTest{
+//    private static Logger log = LoggerFactory.getLogger(Lab_14_Menu_Test.class);
+
     @Test
     public void menuTest(){
+
         new LoginPage(driver)
                 .typeEmail("test@test.com")
                 .typePassword("Test1!")
--- src/test/java/SeleniumBaseTest.java
+++ src/test/java/SeleniumBaseTest.java
@@ -1,10 +1,12 @@
 import config.Config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.LoggerFactory;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -16,7 +18,9 @@ import java.security.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
+import java.util.logging.Logger;
 
+@Slf4j
 public class SeleniumBaseTest {
     protected WebDriver driver;
     protected Config config;
@@ -24,14 +28,19 @@ public class SeleniumBaseTest {
     @BeforeMethod
     public void baseBeforeMethod() {
         System.setProperty("webdriver.chrome.driver",
-                "c:/Tools/drivers/chromedriver.exe");
+                "c:/Tools/drivers/chromedriver.exe"); //jenkins add
+        String url = System.getProperty("applicationUrl");
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        driver.get("http://127.0.0.1:4444");
+        driver.get(url);
 //        driver.get("%applicationUrl%");
 //        driver.get("http://localhost:4444/");
         config = new Config();
+
+        log.info("Tests run on environment" + System.getProperty("ENV"));
+        log.info("Run on url:" + url);
+
     }
 
     @AfterMethod
